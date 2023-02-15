ea54dbebc540a52a6d19f53b02ac70e2013401b8
Thiago Monteiro <tsmonteiro@gmail.com>
2023-02-15 17:05:24 -0300

    client+

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import tercen.util.helper_functions as utl
 from operator_funcs import fit_phate
 # http://127.0.0.1:5402/test/w/047abdf84a6863e86050b9486100ccff/ds/b81b2ae0-e959-4b57-b3e7-19a6e6f6b34d
 # http://127.0.0.1:5402/test/w/047abdf84a6863e86050b9486100ccff/ds/2eb7b60f-8edc-4b03-9795-eb493682c64f
-# tercenCtx = ctx.TercenContext(workflowId="047abdf84a6863e86050b9486100ccff", stepId="b81b2ae0-e959-4b57-b3e7-19a6e6f6b34d",
+# http://127.0.0.1:5402/test/w/047abdf84a6863e86050b9486100ccff/ds/ae4799ab-0312-47dc-a1dd-2b2edef63fec # 2.3Gb
+# tercenCtx = ctx.TercenContext(workflowId="047abdf84a6863e86050b9486100ccff", stepId="ae4799ab-0312-47dc-a1dd-2b2edef63fec",
                     # serviceUri = "http://127.0.0.1:5402/")
 tercenCtx = ctx.TercenContext()
 
